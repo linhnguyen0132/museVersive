b00a78e8be43363f9e8842f3a8443e1a5d8219da
Linh Nguyen <linhnguyen0132@gmail.com>
2026-05-24 03:46:47 -0400

    main to Linh

--- museVersive/src/paintings/starryNight.js
+++ museVersive/src/paintings/starryNight.js
@@ -1,127 +1,60 @@
 import * as THREE from 'three'
 
 export function createStarryNight(scene, loader) {
-
     const objects = {}
 
-    // ======================
-    // SKY DOME
-    // ======================
-
-    const panoTexture =
-        loader.load(
-            '/assets/panoramas/starry_night-pano.jpg'
-        )
-
-    panoTexture.colorSpace =
-        THREE.SRGBColorSpace
-
-    const geometry =
-        new THREE.SphereGeometry(
-            500,
-            60,
-            40,
-            0,
-            Math.PI * 2,
-            0,
-            Math.PI / 2
-        )
-
-    const material =
-        new THREE.MeshBasicMaterial({
-            map: panoTexture,
-            side: THREE.BackSide
-        })
-
-    const dome =
-        new THREE.Mesh(
-            geometry,
-            material
-        )
-
-    scene.add(dome)
-
-    objects.dome = dome
-
-    // ======================
-    // FLOOR
-    // ======================
-
-    const floorGeometry =
-        new THREE.CircleGeometry(
-            30,
-            64
-        )
-
-    const floorMaterial =
-        new THREE.MeshStandardMaterial({
-            color: 0x1a2a44
-        })
-
-    const floor =
-        new THREE.Mesh(
-            floorGeometry,
-            floorMaterial
-        )
-
-    floor.rotation.x =
-        -Math.PI / 2
-
-    floor.position.y =
-        -5
-
-    scene.add(floor)
-
-    objects.floor = floor
-
-    // ======================
-    // TREE FOREGROUND
-    // ======================
-
-//    const treeTexture =
-//        loader.load(
-//            '/assets/painting/starryNight/layers/starry_night-tree.png'
-//        )
-
-//    treeTexture.colorSpace =
-//        THREE.SRGBColorSpace
-        
-//    const treeMaterial =
-//        new THREE.MeshBasicMaterial({
-//            map: treeTexture,
-//            transparent: true,
-//            depthWrite: false
-//        })
-
-//    const treeGeometry =
-//        new THREE.PlaneGeometry(
-//            8,
-//            7.7
-//        )
-
-//    treeGeometry.translate(
-//        0.4,
-//        4.5,
-//        0
-//    )
+    // BACKGROUND (Tout au fond)
+    const bgTexture = loader.load('/assets/painting/starryNight/layers/starry_night-bg.png')
+    bgTexture.colorSpace = THREE.SRGBColorSpace
     
-//    const tree =
-//        new THREE.Mesh(
-//            treeGeometry,
-//            treeMaterial
-//        )
+    const bgMaterial = new THREE.MeshBasicMaterial({ 
+        map: bgTexture 
+    })
+    
+    const bg = new THREE.Mesh(new THREE.PlaneGeometry(8, 9), bgMaterial)
+    bg.position.z = -2 
+    bg.renderOrder = 0 // Dessiné en 1er
+    scene.add(bg)
+    objects.bg = bg
+    
+    // Wave 
+    const spiral1Texture = loader.load('/assets/painting/starryNight/layers/starry_night-wave1.png')
+    spiral1Texture.colorSpace = THREE.SRGBColorSpace
+    
+    const spiral1Material = new THREE.MeshBasicMaterial({
+        map: spiral1Texture,
+        transparent: true,
+        depthWrite: false
+    })
+    const spiral1Geometry = new THREE.PlaneGeometry(8, 7.7)
 
-//    tree.position.set(
-//        0,
-//        -4.5,
-//        -1
-//    )
+    const spiral1 = new THREE.Mesh(spiral1Geometry, spiral1Material)
+    spiral1.position.x = -0.2
+    spiral1.position.y = 0
+    spiral1.position.z = -1.5 
+    spiral1.scale.set(1.15, 1.15, 1)
+    spiral1.renderOrder = 1   
+    scene.add(spiral1)
+    objects.spiral1 = spiral1
 
-//    tree.renderOrder = 2
+    // TREE (Au premier plan)
+    const treeTexture = loader.load('/assets/painting/starryNight/layers/starry_night-tree.png')
+    treeTexture.colorSpace = THREE.SRGBColorSpace
+        
+    const treeMaterial = new THREE.MeshBasicMaterial({
+        map: treeTexture,
+        transparent: true,
+        depthWrite: false 
+    })
 
-//    scene.add(tree)
-
-//    objects.tree = tree
+    const treeGeometry = new THREE.PlaneGeometry(8, 7.7)
+    treeGeometry.translate(0, 4.5, 0)
+    
+    const tree = new THREE.Mesh(treeGeometry, treeMaterial)
+    tree.position.set(0, -4.5, -1) 
+    tree.renderOrder = 2           
+    scene.add(tree)
+    objects.tree = tree
 
     return objects
 }
